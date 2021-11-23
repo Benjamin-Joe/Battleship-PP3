c9574b55e7d36f74d46015febc5f3232b52c9949
Benjamin-Joe <benjaminjoe43@gmail.com>
2021-11-23 20:35:53 +0000

    Create empty functions in strings as a guideline for what is needed, Test out name function in tests.py

--- run.py
+++ run.py
@@ -1,1 +1,24 @@
 # python code goes here
+"""
+
+def print_board(board):
+
+
+def place_ships():
+
+
+def check_ship_fits():
+
+
+def check_for_overlap():
+
+
+def user_input():
+
+
+def hit_counter():
+
+
+def turn_ability():
+
+"""
---
+++ tests.py
@@ -0,0 +1,13 @@
+"""Test file for battleships game"""
+
+
+def name():
+    """
+    A function for getting the user's name
+    """
+    username = str(input("Enter Your Name: "))
+    print("Welcome To Battleships " + str(username))
+    return
+
+
+name()
